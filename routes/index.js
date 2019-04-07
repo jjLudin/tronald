3857b30fe7883975d76b8c7e0910b35a6143a8e0
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2019-04-06 20:14:25 -0700

    Created GET request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+
+const options = {
+  url: 'https://api.tronalddump.io/random/quote',
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Charset': 'utf-8'
+  }
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
+  request(options, function(err, output, body) {
+    var json = JSON.parse(body);
+    res.render('index.html', {
+      quote : json['value'],
+      createdDate : new Date(json['created_at']).toLocaleString(),
+      source : json['_embedded']['source'][0]['url']
+    });
+  });
 });
 
 module.exports = router;
